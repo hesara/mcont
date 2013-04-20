e868823ddb9885b088c5b7cf79ecfcb28f307daf
Henri Sara <hesara@vaadin.com>
2013-04-20 09:34:32 +0300

    Basic project structure and POMs, empty demo, no implementation

---
+++ mcont-demo/src/main/java/org/vaadin/mcont/demo/DemoUI.java
@@ -0,0 +1,30 @@
+package org.vaadin.mcont.demo;
+
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.Title;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.ui.Table;
+import com.vaadin.ui.UI;
+import com.vaadin.ui.VerticalLayout;
+
+@Theme("demo")
+@Title("MCont Add-on Demo")
+@SuppressWarnings("serial")
+public class DemoUI extends UI
+{
+
+    @Override
+    protected void init(VaadinRequest request) {
+    	Table table = new Table();
+    	// TODO create and set container
+    	
+    	final VerticalLayout layout = new VerticalLayout();
+        layout.setStyleName("demoContentLayout");
+        layout.setSizeFull();
+        layout.addComponent(table);
+        setContent(layout);
+
+    }
+
+}
